b69289c62a5be3a523b4d32aec2b6d790dc95f0d
YASME-Tim <hu78sapy@stud.cs.fau.de>
2016-01-03 20:28:39 +0100

    Add compare functions

--- amit.py
+++ amit.py
@@ -18,13 +18,40 @@ def hash_sha256(inbytes):
 	m.update(inbytes)
 	return m.hexdigest()
 
-def hash_print_all(inbytes):
-	print hash_ssdeep(inbytes)
-	print hash_md5(inbytes)
-	print hash_sha1(inbytes)
-	print hash_sha256(inbytes)
+def hash_all(inbytes):
+	a = []
+	a.append(hash_ssdeep(inbytes))
+	a.append(hash_md5(inbytes))
+	a.append(hash_sha1(inbytes))
+	a.append(hash_sha256(inbytes))
+	return a
+
+def compare_ssdeep(hash1, hash2):
+	return ssdeep.compare(hash1, hash2)
+
+def compare_md5(hash1, hash2):
+	return hash1 == hash2
+
+def compare_sha1(hash2, hash1):
+	return hash1 == hash2
+
+def compare_sha256(hash1, hash2):
+	return hash1 == hash2
+
+def compare_all(hasharray1, hasharray2):
+	if len(hasharray1)!=len(hasharray2): return None
+	a = []
+	a.append(compare_ssdeep(hasharray1[0], hasharray2[0]))
+	a.append(compare_md5(hasharray1[1], hasharray2[1]))
+	a.append(compare_sha1(hasharray1[2], hasharray2[2]))
+	a.append(compare_sha256(hasharray1[3], hasharray2[3]))
+	return a
 
 testdata = '\x90'*512*2
-testdata2 = 'mod\x90'*512*2
-hash_print_all(testdata)
-hash_print_all(testdata2)
+testdata2 = 'mod'+'\x90'*512*2
+
+a1 = hash_all(testdata)
+a2 = hash_all(testdata2)
+for i in a1: print i
+for i in a2: print i
+print compare_all(a1, a2)
